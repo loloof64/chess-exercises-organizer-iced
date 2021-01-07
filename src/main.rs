e38907227c8e58e7664cb7fa0f24da4d11843624
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-01-07 19:39:55 +0100

    Initial commit
    Set up iced and a minimal window.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use iced::{Container, Element, Length, Sandbox, Settings, Text};
+
+struct MainWindow {}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {}
+
+impl Sandbox for MainWindow {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn title(&self) -> String {
+        String::from("Chess exercises organizer")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let content = Text::new("Hello World !");
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
+}
+
+pub fn main() -> iced::Result {
+    MainWindow::run(Settings::default())
+}
